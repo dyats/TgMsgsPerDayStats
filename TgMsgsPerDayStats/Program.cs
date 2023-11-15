a18b835739632bf8d354cc3add9a2e3ea1f56a07
Denys <dennyyats@gmail.com>
2023-11-15 22:34:32 +0200

    Improved result format

--- TgMsgsPerDayStats/Program.cs
+++ TgMsgsPerDayStats/Program.cs
@@ -10,4 +10,11 @@ var obj = JsonConvert.DeserializeObject<Root>(jsonText);
 foreach(var msg in obj.Messages.GroupBy(x => DateOnly.FromDateTime(x.Date.Value)))
 {
     Console.WriteLine($"{msg.Key.Month}\\{msg.Key.Day} - {msg.Count()} messages");
+    var groupedByWhom = msg.GroupBy(x => x.From);
+    foreach (var msgFrom in groupedByWhom)
+    {
+        Console.WriteLine($"From: {msgFrom.Key} - {msgFrom.Count()} messages");
+    }
+    Console.WriteLine(groupedByWhom.OrderByDescending(x => x.Count()).First().Key);
+    Console.WriteLine();
 }
