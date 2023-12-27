0d9bb1fda26af8bf9860a8b951553f9d800680c0
Denys <dennyyats@gmail.com>
2023-12-27 18:46:50 +0200

    refactored of chart and rendering logic

---
+++ TgMsgsPerDayStats/ChartService.cs
@@ -0,0 +1,44 @@
+﻿using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot;
+using OxyPlot.SkiaSharp;
+
+namespace TgMsgsPerDayStats;
+
+public static class ChartService
+{
+    public static void GenerateChart(List<DataPoint> messagesForChart)
+    {
+        var plotModel = new PlotModel { Title = "Message Count Over Time" };
+        var lineSeries = new LineSeries
+        {
+            LineJoin = LineJoin.Round,
+            Color = OxyColor.FromArgb(255, 0, 0, 0),
+        };
+        lineSeries.Points.AddRange(messagesForChart);
+        plotModel.Series.Add(lineSeries);
+        plotModel.Background = OxyColor.FromArgb(255, 255, 255, 255);
+
+        plotModel.Axes.Add(new DateTimeAxis
+        {
+            Position = AxisPosition.Bottom,
+            StringFormat = "MM/dd",
+            Title = "Date"
+        });
+        plotModel.Axes.Add(new LinearAxis
+        {
+            Position = AxisPosition.Left,
+            Title = "Number of Messages",
+        });
+
+        GenerateImage(plotModel, "MessageCountChart.png");
+    }
+
+    private static void GenerateImage(IPlotModel plotModel, string filePath)
+    {
+        var exporter = new PngExporter { Width = 1500, Height = 800 };
+
+        using var stream = File.Create(filePath);
+        exporter.Export(plotModel, stream);
+    }
+}
--- TgMsgsPerDayStats/Program.cs
+++ TgMsgsPerDayStats/Program.cs
@@ -1,18 +1,22 @@
-﻿using Newtonsoft.Json;
-using OxyPlot;
-using OxyPlot.Series;
+﻿using OxyPlot;
 using OxyPlot.Axes;
+using System.Text.Json;
 using TgMsgsPerDayStats;
 
-var jsonFilePath = @"/Users/dyats/Downloads/Telegram Desktop/ChatExport_2023-12-22/result.json";
+var jsonFilePath = @"";
 string jsonText = File.ReadAllText(jsonFilePath);
-var obj = JsonConvert.DeserializeObject<Root>(jsonText);
+var jsonSettings = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = false,
+};
+var obj = JsonSerializer.Deserialize<Root>(jsonText);
 
-var messageCounts = new List<DataPoint>();
+var messagesForChart = new List<DataPoint>();
 
 foreach (var msg in obj.Messages.GroupBy(x => DateOnly.FromDateTime(x.Date.Value)))
 {
-    Console.WriteLine($"{msg.Key.Month}\\{msg.Key.Day} - {msg.Count()} messages");
+    var sumOfAllMessages = msg.Count();
+    Console.WriteLine($"{msg.Key.Month}\\{msg.Key.Day} - {sumOfAllMessages} messages");
     var groupedByWhom = msg.GroupBy(x => x.From);
     foreach (var msgFrom in groupedByWhom)
     {
@@ -22,34 +26,7 @@ foreach (var msg in obj.Messages.GroupBy(x => DateOnly.FromDateTime(x.Date.Value
     Console.WriteLine();
 
     var date = msg.Key;
-    messageCounts.Add(new DataPoint(DateTimeAxis.ToDouble(new DateTime(date.Year, date.Month, date.Day)), msg.Count()));
+    messagesForChart.Add(new DataPoint(DateTimeAxis.ToDouble(new DateTime(date.Year, date.Month, date.Day)), sumOfAllMessages));
 }
 
-var plotModel = new PlotModel { Title = "Message Count Over Time" };
-var lineSeries = new LineSeries();
-lineSeries.Points.AddRange(messageCounts);
-plotModel.Series.Add(lineSeries);
-
-plotModel.Axes.Add(new DateTimeAxis
-{
-    Position = AxisPosition.Bottom,
-    StringFormat = "MM\\dd",
-    Title = "Date"
-});
-
-plotModel.Axes.Add(new LinearAxis
-{
-    Position = AxisPosition.Left,
-    Title = "Number of Messages"
-});
-
-ExportPlot(plotModel, "MessageCountGraph.png");
-
-static void ExportPlot(PlotModel plotModel, string filePath)
-{
-    var exporter = new OxyPlot.SkiaSharp.PngExporter { Width = 1000, Height = 500 };
-    using (var stream = File.Create(filePath))
-    {
-        exporter.Export(plotModel, stream);
-    }
-}
+ChartService.GenerateChart(messagesForChart);
